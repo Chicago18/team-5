04c4da96fb041191ddda15778cba5dcea2dfdb45
Huong Dang <huongd97@knights.ucf.edu>
2018-10-13 02:00:15 -0500

    Finally have firebase create and sign up!!!

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -2,7 +2,7 @@ import {Component} from "@angular/core";
 import {NavController} from "ionic-angular";
 import {LoginPage} from "../login/login";
 import {Validators, FormBuilder, FormGroup } from "@angular/forms";
-
+import {AuthProvider} from "../../providers/auth/auth";
 
 @Component({
   selector: 'page-register',
@@ -10,7 +10,8 @@ import {Validators, FormBuilder, FormGroup } from "@angular/forms";
 })
 export class RegisterPage {
   private signUpForm : FormGroup;
-  constructor(public nav: NavController, private formBuilder: FormBuilder) {
+
+  constructor(public nav: NavController, private formBuilder: FormBuilder, public authProvider: AuthProvider) {
     this.signUpForm = this.formBuilder.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
@@ -22,6 +23,8 @@ export class RegisterPage {
   // register and go to home page
   register() {
     console.log('this.signUpForm', this.signUpForm.value);
+    const {firstName, lastName, email, password} = this.signUpForm.value;
+    this.authProvider.signUpUser(email, password, firstName, lastName);
   }
 
   // go to login page
